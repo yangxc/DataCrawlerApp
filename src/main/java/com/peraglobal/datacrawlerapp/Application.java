3fc7d11eabb453224f1fdfb635d6598a40ccc690
yongqian.liu <yongqian.liu@peraglobal.com>
2017-01-11 14:14:08 +0800

    优化代码

--- src/main/java/com/peraglobal/datacrawlerapp/Application.java
+++ src/main/java/com/peraglobal/datacrawlerapp/Application.java
@@ -7,17 +7,36 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * <code>Application.java</code>
+ * <p>
+ * 功能:启动类
+ * 
+ * <p>
+ * Copyright 安世亚太 2016 All right reserved.
+ * 
+ * @author yongqian.liu
+ * @version 1.0 2016-12-2 </br>
+ * 			最后修改人 无
+ */
 @SpringBootApplication
-@EnableConfigurationProperties({WebServiceProperties.class})
-public class DataCrawlerApp {
+@EnableConfigurationProperties({ WebServiceProperties.class })
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataCrawlerApp.class, args);
+		// 设置启动类
+		SpringApplication.run(Application.class, args);
 	}
-	
+
+	/**
+	 * 设置 RestTemplate 功能，在项目中调用 RESTFul API
+	 * 
+	 * @param builder
+	 * @return
+	 */
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
-	
+
 }
