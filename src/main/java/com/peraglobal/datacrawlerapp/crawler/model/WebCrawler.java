d415b93bae55aea18a16076df47d6590c42a90ac
yongqian.liu <eyongqian.liu@peraglobal.com>
2017-02-23 15:59:05 +0800

    增加：代理功能

--- src/main/java/com/peraglobal/datacrawlerapp/crawler/model/WebCrawler.java
+++ src/main/java/com/peraglobal/datacrawlerapp/crawler/model/WebCrawler.java
@@ -3,6 +3,9 @@ package com.peraglobal.datacrawlerapp.crawler.model;
 public class WebCrawler extends Crawler {
 
 	private WebRule webRule;
+	
+	private Proxy proxy;
+
 
 	public WebRule getWebRule() {
 		return webRule;
@@ -11,4 +14,13 @@ public class WebCrawler extends Crawler {
 	public void setWebRule(WebRule webRule) {
 		this.webRule = webRule;
 	}
+	
+	public Proxy getProxy() {
+		return proxy;
+	}
+
+	public void setProxy(Proxy proxy) {
+		this.proxy = proxy;
+	}
+	
 }
